eeb881a3ed78030ac38bc14d4f94a339631cf7ba
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-02 20:07:42 +0700

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -5,11 +5,11 @@ from config import SUPPORT_CHAT
 
 keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [InlineKeyboardButton(text="Ambil String Disini", callback_data="gensession")],
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+                text="Jasa Si Arab", url="https://t.me/SiArab_Store"
             ),
         ],
     ]
