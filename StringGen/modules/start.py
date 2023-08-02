e61b3f4c06c40fa201e7c9d41496dbbc8f8423c7
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-02 20:04:17 +0700

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+        text=f"Halo Mek {message.from_user.first_name},\n Nih Bot Ambil String {Anony.mention},\nKalo lu hoki yah akun lu ga kedeak Kalo Kedeak jangan salahin gua.",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
