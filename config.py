fb112f802328e453b72be0d6844b908ab387db8a
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-04 01:27:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 1345594412))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SiArabSupport")
+BOT_USERNAME = gatenv("BOT_USERNAME", "BabuArabRobot")
